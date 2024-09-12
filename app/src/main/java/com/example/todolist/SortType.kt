a39dda30e4edb28e2a227ef14dffc66c9fc2df85
FatmaAtta <fatmamaali@gmail.com>
2024-09-12 20:51:52 +0300

    Created interface responsible for different events, enum class for the different sort types

---
+++ app/src/main/java/com/example/todolist/SortType.kt
@@ -0,0 +1,7 @@
+package com.example.todolist
+
+enum class SortType {
+    PRIORITY,
+    DUE_DATE,
+    IS_COMPLETED,
+}
---
+++ app/src/main/java/com/example/todolist/TODOEvent.kt
@@ -0,0 +1,17 @@
+package com.example.todolist
+
+import java.time.LocalDateTime
+
+//contains anything related to user interaction, clicking any button
+sealed interface TODOEvent{
+    object SaveTODO: TODOEvent
+    data class SetTODOName(val TODOName: String): TODOEvent
+    data class SetTODODesc(val TODODesc: String): TODOEvent
+    data class SetTODOPriority(val TODOPriority: Int): TODOEvent
+    data class SetTODODueDate(val TODODueDate: LocalDateTime): TODOEvent
+    data class SetTODOIsComp(val TODOIsComp: Boolean): TODOEvent
+    data class SortTODO(val sortType: SortType): TODOEvent
+    data class DeleteTODO(val TODO: TODO): TODOEvent
+    object SowDialogue: TODOEvent
+    object HideDialogue: TODOEvent
+}
