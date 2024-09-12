36c17c05c9a5ec73f6751c8a9fbbdc90d12da44d
FatmaAtta <fatmamaali@gmail.com>
2024-09-12 16:09:21 +0300

    Created TODO data class

---
+++ app/src/main/java/com/example/todolist/TODO.kt
@@ -0,0 +1,16 @@
+package com.example.todolist
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDateTime
+
+@Entity
+data class TODO(
+    var taskName: String,
+    var taskDesc: String,
+    var Priority: Int,
+    var dueDate: LocalDateTime,
+    var isCompleted: Boolean,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+)
